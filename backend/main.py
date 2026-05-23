9b2892490f8ad80e3272462ee5858e95dd29e070
Deepa Saxena <saxenadeepa103@gmail.com>
2026-05-23 21:23:41 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -29,10 +29,8 @@ app = FastAPI(
 # =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://levels-val.vercel.app",
-        "http://localhost:3000"
-    ],
+    allow_origins=["http://localhost:3000"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
